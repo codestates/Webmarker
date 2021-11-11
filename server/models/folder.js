b858544025b7ab545c5cd13b4266caa0215dad43
SiwooPak <bogus1620@naver.com>
2021-11-11 16:31:12 +0900

    Feat: Setup DB 시퀄라이즈 초기화 및 모델, 마이그레이션 파일 생성 및 각 모덴 간의 관계 설정 세팅

---
+++ server/models/folder.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Folder extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Folder.init({
+    id: {
+      type: DataTypes.Number,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Folder',
+  });
+  return Folder;
+};
