60f4f4fb6a0d8163fc6af401aed8c28e47a1e96b
rec.fffixing <imdex1009@gmail.com>
2021-11-11 14:22:52 +0900

    Feat: Add tokenFunctions 토큰 생성 및 토큰 확인과 관련된 함수 작성 완료
    ! Location
    - server/controller/tokenFunction.js
    
    ! Causion: 모듈 업데이트
    - cookie-parser 추가
    
    ! index.js 수정
    - morgan 활성화
    - token 테스트 목적 테스팅 코드 일부 작성

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const fs = require("fs");
 const https = require("https");
 const cors = require("cors");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const express = require("express");
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 const HTTP_PORT = process.env.HTTP_PORT || 80;
 const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -17,10 +20,42 @@ app.use(
     credentials: true,
   })
 );
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.status(200).send("hello!");
 });
+
+// ! 아래는 토큰 관련 함수를 테스트하기 위한 라우트 ~ @end 까지
+// 아래 코드는 추후 재활용 또는 폐기 하도록 하겠습니다.
+// const {
+//   generateAccessToken,
+//   checkAccessToken,
+//   generateRefreshToken,
+//   checkRefreshToken,
+// } = require("./controller/tokenFunction");
+
+// app.post("/takeToken", (req, res) => {
+//   const data = req.body;
+//   const accessToken = generateAccessToken(data);
+//   const refreshToken = generateRefreshToken(data);
+//   res
+//     .status(201)
+//     .cookie("refreshToken", refreshToken, {
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "none",
+//       maxAge: 60 * 60 * 24 * 7, // 최대 기간 : 1주일 설정
+//     })
+//     .send({ data: { accessToken }, message: "ok" });
+// });
+// app.get("/takeToken", (req, res) => {
+//   const result = checkAccessToken(req);
+//   const refresh = checkRefreshToken(req);
+//   res.status(200).send({ A: result, B: refresh });
+// });
+
+// !@end 여기서 테스트 기능 종료
 
 let server;
 if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
