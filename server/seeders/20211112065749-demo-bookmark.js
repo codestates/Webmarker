a1561b10ceaee6172429220f8aa07343584d001d
rec.fffixing <imdex1009@gmail.com>
2021-11-12 17:12:37 +0900

    Feat: Set pipeline
    ! appspec.yml, scripts files added

---
+++ server/seeders/20211112065749-demo-bookmark.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert("Bookmarks", [
+      {
+        title: "NAVER!",
+        url: "www.naver.com",
+        content: "세상의 모든 지식",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete("Bookmarks", null, {});
+  },
+};
