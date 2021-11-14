2d7b9e6542526eb52b1772fa76b95631f8d4bc17
SiwooPak <bogus1620@naver.com>
2021-11-15 08:21:34 +0900

    Feat: Add folder api 폴더의 라우팅 경로 추가 및 api 기능 구현현

--- server/router/directions/foldersRouter.js
+++ server/router/directions/foldersRouter.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const controller = require("./../../controller");
 
 router.get("/", controller.folders.test);
-
+router.post("/", controller.folders.createFolder);
+router.put("/", controller.folders.updateFolderName);
+router.delete("/", controller.folders.deleteFolder);
 module.exports = router;
