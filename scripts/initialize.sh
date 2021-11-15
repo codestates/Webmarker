24c44820db6b1434b6e3748b0a3c83832cd288cc
rec.fffixing <imdex1009@gmail.com>
2021-11-15 17:18:14 +0900

    hotfix: edit initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/Webmarker/server
+sudo -i
 npm install
 npm install pm2@latest -g
+logout
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-
 
