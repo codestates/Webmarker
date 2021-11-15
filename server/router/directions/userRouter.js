01dec5339a4832f79af3dd11d95d76e54972eae8
rec.fffixing <imdex1009@gmail.com>
2021-11-15 21:14:12 +0900

    Feat: fix api method
    ! POST /users/password 부분 메소드 수정
    POST => PATCH

--- server/router/directions/userRouter.js
+++ server/router/directions/userRouter.js
@@ -6,7 +6,7 @@ router.post("/signup", controller.users.signup);
 router.get("/login", controller.users.login);
 router.get("/userinfo", controller.users.userInfo);
 router.get("/password", controller.users.checkPassword);
-router.post("/password", controller.users.updatePassword);
+router.patch("/password", controller.users.updatePassword);
 router.delete("/", controller.users.withdrawal);
 
 module.exports = router;
