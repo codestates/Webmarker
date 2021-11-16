6572bb763220031365503d92af485bf293bfaf29
rec.fffixing <imdex1009@gmail.com>
2021-11-16 09:29:40 +0900

    Edit : login method changed
    - GET => POST

--- server/router/directions/userRouter.js
+++ server/router/directions/userRouter.js
@@ -3,7 +3,7 @@ const controller = require("./../../controller/index");
 
 router.get("/", controller.users.test);
 router.post("/signup", controller.users.signup);
-router.get("/login", controller.users.login);
+router.post("/login", controller.users.login);
 router.get("/userinfo", controller.users.userInfo);
 router.get("/password", controller.users.checkPassword);
 router.patch("/password", controller.users.updatePassword);
