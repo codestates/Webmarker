6f24d8bb141164d59894d03c003258c2987d42fc
rec.fffixing <imdex1009@gmail.com>
2021-11-17 20:16:18 +0900

    Edit : edit start.sh
    add google Oauth ENV

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,7 @@ export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --name
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_SECRET_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_SECRET_ID --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start index.js
