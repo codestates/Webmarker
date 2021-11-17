63c4d3b327fa85ed020340e6d67620c7d94083d5
rec.fffixing <81807607+imdex1009@users.noreply.github.com>
2021-11-17 23:45:27 +0900

    Revert "tesing"
    This reverts commit b7062307fe319444c667cb988a9f51d90f6c9532.

--- server/controller/details/users.js
+++ server/controller/details/users.js
@@ -194,7 +194,6 @@ module.exports = {
       }
       res
         .cookie("accessToken", accessToken, {
-          domain: "webmarker.link",
           maxAge: 24 * 6 * 60 * 10000,
           httpOnly: true,
           secure: true,
