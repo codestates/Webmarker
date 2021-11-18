ce7ee8d5c3958f9ec31d49cb2c747940edc5dc82
SiwooPak <bogus1620@naver.com>
2021-11-18 11:54:27 +0900

    Fix : Modify moveBookmark !bookmarkrouter moveBookmark :folderId -> id /controller/details/users.js edit: movebookmark.js

--- server/router/directions/bookmarksRouter.js
+++ server/router/directions/bookmarksRouter.js
@@ -4,7 +4,7 @@ const controller = require("./../../controller/index");
 router.get("/", controller.bookmarks.allBookmarks);
 router.post("/", controller.bookmarks.addBookmark);
 router.put("/", controller.bookmarks.updateBookmark);
-router.patch("/:folderId", controller.bookmarks.moveBookmark);
+router.patch("/:id", controller.bookmarks.moveBookmark);
 router.delete("/:id", controller.bookmarks.deleteBookmark);
 
 module.exports = router;
